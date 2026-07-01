9ab5bac5c8bc94b0b53b628fd6ca0e2a845584c9
mmn1998 <mm.nomani.1998@gmail.com>
2026-07-02 01:05:41 +0330

    adding some fake tests

--- tests/TopUpSimulation.Tests/UnitTest1.cs
+++ tests/TopUpSimulation.Tests/UnitTest1.cs
@@ -3,8 +3,26 @@ namespace TopUpSimulation.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task SaveTransaction_ShouldPersistIntoDatabase()
         {
+            Assert.True(true);
+        }
+        [Fact]
+        public async Task Worker_ShouldPublishEvent_WhenTransactionSucceeded()
+        {
+            Assert.True(true);
+
+        }
+        [Fact]
+        public async Task KafkaConsumer_ShouldDispatchIncomingEvent()
+        {
+            Assert.True(true);
+
+        }
+        [Fact]
+        public async Task OutboxProcessor_ShouldPublishPendingMessages()
+        {
+            Assert.True(true);
 
         }
     }
