4a62a95d33cddfdbfb7dddcbc6fd459c0fe98923
mmn1998 <mm.nomani.1998@gmail.com>
2026-07-02 01:02:39 +0330

    fixing a tiny bug :))

--- src/Infrastructure/TopUpSimulation.Persistence/Extensions/ConfigurationExtension.cs
+++ src/Infrastructure/TopUpSimulation.Persistence/Extensions/ConfigurationExtension.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Sima.Framework.Core.Repository;
 using SIMA.Framework.Infrastructure.Data;
+using System;
 using TopUpSimulation.Domain.Models.Outboxes.Contracts;
+using TopUpSimulation.Framework.Common.Exceptions;
+using TopUpSimulation.Persistence.Contexts;
 using TopUpSimulation.Persistence.Repositories;
 
 namespace TopUpSimulation.Persistence.Extensions;
@@ -11,6 +15,10 @@ public static class ConfigurationExtension
 {
     public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<DbContext, TopUpDbContext>(options =>
+        {
+            options.UseInMemoryDatabase(configuration.GetValue<string>("InMemoryDbName") ?? throw TopUpResultException.ConfigureError);
+        });
         services.AddScoped<ITransactionRepository, TransactionRepository>();
         services.AddScoped<IOutBoxRepository, OutBoxRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
